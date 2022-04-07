480d5db34739316e6584d97899ff12691f8ac4ff
Mikko Suhonen <mikko.suhonen@metropolia.fi>
2022-04-07 12:53:09 +0300

    Initial Commit

---
+++ RestaurantFinder/RestaurantFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantFinderApp.swift
+//  RestaurantFinder
+//
+//  Created by iosdev on 7.4.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
