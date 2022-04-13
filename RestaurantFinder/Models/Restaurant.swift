a45e509c5e5d99e6ba2261b6022a0c0da0438a87
Mikko Suhonen <mikko.suhonen@metropolia.fi>
2022-04-13 17:47:35 +0300

    Details-view added

--- RestaurantFinder/Models/Restaurant.swift
+++ RestaurantFinder/Models/Restaurant.swift
@@ -2,7 +2,32 @@
 //  Restaurant.swift
 //  RestaurantFinder
 //
-//  Created by iosdev on 12.4.2022.
+//  Created by Mikko Suhonen on 12.4.2022.
 //
 
 import Foundation
+
+struct Restaurant: Identifiable {
+    let id: UUID
+    var name: String
+    var imageURL: String
+    var rating: Int
+    
+    init(id: UUID = UUID(), name: String, imageURL: String, rating: Int) {
+        self.id = id
+        self.name = name
+        self.imageURL = imageURL
+        self.rating = rating
+    }
+}
+
+extension Restaurant {
+    static let sampleData: [Restaurant] =
+    [
+        Restaurant(name: "Pago Restaurant", imageURL: "https://media-cdn.tripadvisor.com/media/photo-t/15/7a/f4/02/pago-balcony.jpg", rating: 4),
+        Restaurant(name: "The Owl House", imageURL: "https://media-cdn.tripadvisor.com/media/photo-t/15/7a/f4/02/pago-balcony.jpg", rating: 3),
+        Restaurant(name: "Cella", imageURL: "https://media-cdn.tripadvisor.com/media/photo-t/15/7a/f4/02/pago-balcony.jpg", rating: 5),
+        Restaurant(name: "THE GRILL", imageURL: "https://media-cdn.tripadvisor.com/media/photo-t/15/7a/f4/02/pago-balcony.jpg", rating: 3),
+    ]
+    
+}
