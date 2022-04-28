f2021e73e9e7bb09e4fdcc992186c2da2115bf29
Mikko Suhonen <mikko.suhonen@metropolia.fi>
2022-04-28 23:47:48 +0300

    deleting favorites added

---
+++ RestaurantFinder/ListingElement.swift
@@ -0,0 +1,62 @@
+//
+//  ListingElement.swift
+//  RestaurantFinder
+//
+//  Created by Mikko on 28.4.2022.
+//
+
+import SwiftUI
+import CoreLocation
+import CoreData
+
+struct ListingElement: View {
+    
+    var restaurants : FetchedResults<Restaurant>
+    
+    let colors: [Color] = [.customRed, .customBlue, .customGreen, .customOrange, .customYellow]
+    
+    let listLength: Int = 15
+    
+    /*
+    var listing: [String: FetchedResults<Restaurant>] = [
+        "near" : restaurants,
+        "ratings" : restaurantsByRating,
+        "price" : restaurants
+    ]*/
+    
+    //var showInListing = listing[sortedBy]
+    
+    var body: some View {
+        ScrollView(.horizontal, content:{
+            HStack{
+                Spacer()
+                    .frame(width: 10)
+                ForEach(restaurants.prefix(listLength) ?? []){ restaurant in
+                    let latitude = Double(restaurant.latitude ?? "60.16364")
+                    let longitude = Double(restaurant.longitude ?? "24.947996")
+                    let newRestaurant = RestaurantHC(
+                        name: restaurant.name ?? "no name",
+                        imageURL: restaurant.url ?? "no",
+                        rating: restaurant.rating,
+                        description: restaurant.desc ?? "no descpription",
+                        address: restaurant.address ?? "no address",
+                        priceLevel: restaurant.price,
+                        coordinate: CLLocationCoordinate2D(
+                            latitude: latitude ?? 60.16364,
+                            longitude: longitude ?? 24.947996)
+                    )
+                    NavigationLink(destination: DetailsView(restaurant: newRestaurant)) {
+                        ListElementView(restaurant: restaurant, color: colors.randomElement() ?? .customBlue)
+                    }
+                }
+            }
+        }
+        )
+    }
+}
+
+struct ListingElement_Previews: PreviewProvider {
+    static var previews: some View {
+        ListingElement(sortedBy: "near")
+    }
+}
